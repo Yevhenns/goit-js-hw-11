9c87e058c7f31efae274af7abb68c466254e05de
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-09-29 21:02:23 +0300

    Update apiService.js

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
 
 const API_KEY = '30263360-410abfd80804834cbd5c2123c';
-const DATA_URL = 'https://pixabay.com/api/';
+const DATA_URL = 'https://pixabay.com/api';
+
+axios.defaults.baseURL = DATA_URL;
