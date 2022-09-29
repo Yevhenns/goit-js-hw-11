fa975970af77099eb7698f853ad10ce573827a92
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-09-29 18:43:32 +0000

    Deploying to gh-pages from @ Yevhenns/goit-js-hw-11@758af1d2352aad177574382980bf75c76ac95fab 🚀

--- index.561a6523.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector("#search-form"),input:document.querySelector('[name="searchQuery"]'),btn:document.querySelector('[type="submit"]')};e.form.addEventListener("submit",(async function(t){t.preventDefault();const n=e.input.value;console.log(n)}));
-//# sourceMappingURL=index.561a6523.js.map
