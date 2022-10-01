a366c6352b931a858888ae7be1ce2076b485f85c
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-01 11:13:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,11 +13,10 @@ refs.form.addEventListener('submit', onFormSubmit);
 async function onFormSubmit(evt) {
   evt.preventDefault();
 
-  const searchQuery = refs.input.value;
+  const searchQuery = evt.currentTarget.searchQuery.value;
   console.log(searchQuery);
 
   const photos = await getPhotos(searchQuery, 1);
-  console.log(photos);
 
   refs.gallery.innerHTML = createGalleryMarkup(photos);
 }
