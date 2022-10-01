fdec2fdfaaec6933a989b8de34e05aa664d224c1
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-01 11:13:07 +0300

    Update markup.js

--- src/utils/markup.js
+++ src/utils/markup.js
@@ -1,4 +1,4 @@
-import { getPhotos } from './apiService';
+// import { getPhotos } from './apiService';
 
 export function createGalleryMarkup(photos) {
   return photos
@@ -12,22 +12,23 @@ export function createGalleryMarkup(photos) {
         downloads,
       }) => {
         return `<div class="photo-card">
-      <a fref=${largeImageURLtags}>
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views${views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads${downloads}</b>
-    </p>
-  </div></a>
+        <a fref="${largeImageURLtags}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes ${likes}</b>
+            </p>
+            <p class="info-item">
+              <b>Views ${views}</b>
+            </p>
+            <p class="info-item">
+              <b>Comments ${comments}</b>
+            </p>
+            <p class="info-item">
+              <b>Downloads ${downloads}</b>
+            </p>
+          </div>
+        </a>
   </div>`;
       }
     )
