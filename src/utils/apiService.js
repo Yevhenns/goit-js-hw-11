b8b6bd82d311ac7875f52968891c9913e39dfe41
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-01 12:26:24 +0300

    Update apiService.js

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -3,13 +3,22 @@ import axios from 'axios';
 const API_KEY = '30263360-410abfd80804834cbd5c2123c';
 const DATA_URL = 'https://pixabay.com/api';
 
-axios.defaults.baseURL = DATA_URL;
+// axios.defaults.baseURL = DATA_URL;
+
+const agent = axios.create({
+  baseURL: 'https://pixabay.com/api',
+  params: {
+    key: '30263360-410abfd80804834cbd5c2123c',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 40,
+  },
+});
 
 export async function getPhotos(searchQuery, page) {
   const {
     data: { hits },
-  } = await axios.get(
-    `/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
-  );
+  } = await agent.get(`?q=${searchQuery}&page=${page}`);
   return hits;
 }
