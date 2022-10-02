3354b0160942f823f1e3bfe4f193a37fcc66661c
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-02 09:46:09 +0300

    Update markup.js

--- src/utils/markup.js
+++ src/utils/markup.js
@@ -1,35 +1,35 @@
-export function createGalleryMarkup(photos) {
-  return photos
-    .map(
-      ({
-        webformatURL,
-        largeImageURLtags,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<div class="photo-card">
-        <a fref="${largeImageURLtags}">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-          <div class="info">
-            <p class="info-item">
-              <b>Likes ${likes}</b>
-            </p>
-            <p class="info-item">
-              <b>Views ${views}</b>
-            </p>
-            <p class="info-item">
-              <b>Comments ${comments}</b>
-            </p>
-            <p class="info-item">
-              <b>Downloads ${downloads}</b>
-            </p>
-          </div>
-        </a>
-  </div>`;
-      }
-    )
-    .join('');
-}
+// export function createGalleryMarkup(photos) {
+//   return photos
+//     .map(
+//       ({
+//         webformatURL,
+//         largeImageURLtags,
+//         tags,
+//         likes,
+//         views,
+//         comments,
+//         downloads,
+//       }) => {
+//         return `<div class="photo-card">
+//         <a fref="${largeImageURLtags}">
+//         <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//           <div class="info">
+//             <p class="info-item">
+//               <b>Likes ${likes}</b>
+//             </p>
+//             <p class="info-item">
+//               <b>Views ${views}</b>
+//             </p>
+//             <p class="info-item">
+//               <b>Comments ${comments}</b>
+//             </p>
+//             <p class="info-item">
+//               <b>Downloads ${downloads}</b>
+//             </p>
+//           </div>
+//         </a>
+//   </div>`;
+//       }
+//     )
+//     .join('');
+// }
