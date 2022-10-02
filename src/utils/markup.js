24474b8407842719fb563252bf09b35764a5251e
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-02 10:44:34 +0300

    Update markup.js

--- src/utils/markup.js
+++ src/utils/markup.js
@@ -1,39 +1,3 @@
-// export function createGalleryMarkup(photos) {
-//   return photos
-//     .map(
-//       ({
-//         webformatURL,
-//         largeImageURLtags,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       }) => {
-//         return `<div class="photo-card">
-//         <a fref="${largeImageURLtags}">
-//         <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//           <div class="info">
-//             <p class="info-item">
-//               <b>Likes ${likes}</b>
-//             </p>
-//             <p class="info-item">
-//               <b>Views ${views}</b>
-//             </p>
-//             <p class="info-item">
-//               <b>Comments ${comments}</b>
-//             </p>
-//             <p class="info-item">
-//               <b>Downloads ${downloads}</b>
-//             </p>
-//           </div>
-//         </a>
-//   </div>`;
-//       }
-//     )
-//     .join('');
-// }
-
 export default function renderCard(data) {
   return `<div class="photo-card">
     <a href="${largeImageURL}">
