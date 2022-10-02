4e8264dff4a27db9b10a678b82fa3f45a2ce3bb4
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-02 10:39:12 +0300

    Update apiService.js

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -1,41 +1,3 @@
-// import axios from 'axios';
-
-// const agent = axios.create({
-//   baseURL: 'https://pixabay.com/api',
-//   params: {
-//     key: '30263360-410abfd80804834cbd5c2123c',
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//     per_page: 40,
-//   },
-// });
-
-// export default class apiService {
-//   constructor() {
-//     this.searchQuery = '';
-//     this.page = 1;
-//   }
-//   async getPhotos() {
-//     const {
-//       data: { hits },
-//     } = await agent.get(`?q=${this.searchQuery}&page=${this.page}`);
-//     if (!hits.length) {
-//       throw new Error();
-//     }
-//     return hits;
-//   }
-//   get query() {
-//     return this.searchQuery;
-//   }
-//   set query(newSearchQuery) {
-//     this.searchQuery = newSearchQuery;
-//   }
-//   incrementPage() {
-//     this.page += 1;
-//   }
-// }
-
 import axios from 'axios';
 
 export default class NewsApiService {
@@ -62,8 +24,6 @@ export default class NewsApiService {
       const response = await axios(axiosOptions);
 
       const data = response.data;
-      // console.log(data)
-      this.incrementPage();
       return data;
     } catch (error) {
       console.error(error);
