c7254dae77004ddd972936ca68d20b17462341a3
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-02 10:21:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,56 +1,3 @@
-// import apiService from './utils/apiService';
-// import { createGalleryMarkup } from './utils/markup';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// import { Notify } from 'notiflix';
-
-// const refs = {
-//   form: document.querySelector('#search-form'),
-//   input: document.querySelector('[name="searchQuery"]'),
-//   btn: document.querySelector('[type="submit"]'),
-//   gallery: document.querySelector('.gallery'),
-//   loadMoreBtn: document.querySelector('.load-more'),
-// };
-
-// const lightbox = new SimpleLightbox('.gallery a', {
-//   captions: true,
-//   captionPosition: 'bottom',
-//   captionDelay: 250,
-//   animationSpeed: 250,
-// });
-// console.log(lightbox);
-
-// const apiPhotoService = new apiService();
-
-// refs.form.addEventListener('submit', onFormSubmit);
-
-// async function onFormSubmit(evt) {
-//   evt.preventDefault();
-
-//   const searchQuery = evt.currentTarget.searchQuery.value;
-
-//   apiPhotoService.query = searchQuery;
-//   apiPhotoService.page = 1;
-//   try {
-//     const photos = await apiPhotoService.getPhotos();
-//     refs.gallery.innerHTML = createGalleryMarkup(photos);
-//     lightbox.refresh();
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-// refs.loadMoreBtn.addEventListener('click', loadMore);
-
-// async function loadMore() {
-//   apiPhotoService.incrementPage();
-//   const photos = await apiPhotoService.getPhotos();
-//   refs.gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(photos));
-//   lightbox.refresh();
-// }
-
-// _________________
-
 import './sass/index.scss';
 
 import NewsApiService from './utils/apiService';
@@ -71,6 +18,9 @@ const refs = {
   galleryContainer: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
 };
+
+refs.loadMoreBtn.classList.add('is-hidden');
+
 let isShown = 0;
 const newsApiService = new NewsApiService();
 
@@ -113,7 +63,7 @@ function onLoadMore() {
 }
 
 async function fetchGallery() {
-  refs.loadMoreBtn.classList.add('is-hidden');
+  // refs.loadMoreBtn.classList.add('is-hidden');
 
   const r = await newsApiService.fetchGallery();
   const { hits, total } = r;
