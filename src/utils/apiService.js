423051298f18051c43cd06d14c82568d0d2a307d
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-10-02 09:46:07 +0300

    Update apiService.js

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -1,37 +1,37 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const agent = axios.create({
-  baseURL: 'https://pixabay.com/api',
-  params: {
-    key: '30263360-410abfd80804834cbd5c2123c',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 40,
-  },
-});
+// const agent = axios.create({
+//   baseURL: 'https://pixabay.com/api',
+//   params: {
+//     key: '30263360-410abfd80804834cbd5c2123c',
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//     per_page: 40,
+//   },
+// });
 
-export default class apiService {
-  constructor() {
-    this.searchQuery = '';
-    this.page = 1;
-  }
-  async getPhotos() {
-    const {
-      data: { hits },
-    } = await agent.get(`?q=${this.searchQuery}&page=${this.page}`);
-    if (!hits.length) {
-      throw new Error();
-    }
-    return hits;
-  }
-  get query() {
-    return this.searchQuery;
-  }
-  set query(newSearchQuery) {
-    this.searchQuery = newSearchQuery;
-  }
-  incrementPage() {
-    this.page += 1;
-  }
-}
+// export default class apiService {
+//   constructor() {
+//     this.searchQuery = '';
+//     this.page = 1;
+//   }
+//   async getPhotos() {
+//     const {
+//       data: { hits },
+//     } = await agent.get(`?q=${this.searchQuery}&page=${this.page}`);
+//     if (!hits.length) {
+//       throw new Error();
+//     }
+//     return hits;
+//   }
+//   get query() {
+//     return this.searchQuery;
+//   }
+//   set query(newSearchQuery) {
+//     this.searchQuery = newSearchQuery;
+//   }
+//   incrementPage() {
+//     this.page += 1;
+//   }
+// }
